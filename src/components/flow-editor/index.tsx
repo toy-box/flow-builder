2a89f5818fc2768ba66361289b3e07ce79a75fb8
shopnb <575994260@qq.com>
2021-10-22 13:59:47 +0800

    bugfix

--- src/components/flow-editor/index.tsx
+++ src/components/flow-editor/index.tsx
@@ -203,7 +203,7 @@ export const FlowEditor: FC<{ flowMeta: FlowGraphMeta }> = ({ flowMeta }) => {
 
   useEffect(() => {
     MetaObjectService.metaObjectData().then(({data}) => {
-      initRegisters(data);
+      initRegisters(data.content);
     })
   }, [initFieldMetas, initFieldServices, initRegisters])
 
